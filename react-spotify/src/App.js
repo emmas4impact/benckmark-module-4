9a37f2821db49f55bddab78646e4340d0eaf2341
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-12 12:02:10 +0300

    footer and main content added

--- react-spotify/src/App.js
+++ react-spotify/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import HomePage from './component/HomePage/HomePage';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Sidebar from './component/SideBar/SideBar'
+import Sidebar from './component/SideBar/SideBar';
+import FooterPage  from './component/Footer/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+     <Sidebar />
      <HomePage />
+     <FooterPage />
      
     </div>
   );
