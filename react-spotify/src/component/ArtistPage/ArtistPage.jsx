cb8e752267f24bf17fea8e1f7a5debfce5e0997e
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-14 00:36:20 +0300

    artistpage done

--- react-spotify/src/component/ArtistPage/ArtistPage.jsx
+++ react-spotify/src/component/ArtistPage/ArtistPage.jsx
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
-
+import { Col, Image, Modal, Button, Spinner } from "react-bootstrap";
 class ArtistPage extends Component {
     
     state = {
         albums: [],
-        tracks: [],
+        tracksresponse: [],
         artistName: [],
         loading: true
     }
     componentDidMount = () => {
-        const albumId = this.props.match.params.id;
+        const artistId = this.props.match.params.id;
         let headers = new Headers({
             "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
             "x-rapidapi-key":
               "8275c582bamshd83a3179dd00459p19f0b2jsn94c889368579",
           });
   
-          fetch("https://deezerdevs-deezer.p.rapidapi.com/album/" + albumId, {
+          fetch("https://deezerdevs-deezer.p.rapidapi.com/artist/" + artistId, {
                 "method": "GET",
                 "headers": {
                     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
@@ -26,18 +26,48 @@ class ArtistPage extends Component {
             .then(response => {
                return response.json()
               
-            }).then((album)=>{
+            }).then((artist)=>{
                 //console.log("response from fetch", response.json());
-                let albums = album
-                let tracks = album.tracks.data
-                const artistName = album.artist
-                this.setState({
-                   
-                    artistName: artistName,
-                    loading: false
-                })
+                //let albums = album
+               // let tracks = album.tracks.data
+               const artistName = artist
+               this.setState({
+                artistName: artistName,
+                
+                loading: false
+            })
               
-                console.log('new state album', this.state.artistName)
+               fetch("https://deezerdevs-deezer.p.rapidapi.com/artist/" +
+                  artistId +
+                  "/top?limit=50",
+                {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+                        "x-rapidapi-key": "8275c582bamshd83a3179dd00459p19f0b2jsn94c889368579"
+                    }
+                }
+              ).then(response=>{
+                  if(response.ok){
+                      return response.json()
+                  }
+              }).then(tracklist=>{
+                  let track = tracklist.data;
+                  
+                  this.setState({
+                   
+                    tracksresponse: track,
+                   
+                })
+                console.log('new state track response', this.state.tracksresponse)
+              })
+              
+              
+              
+             
+              
+                console.log('new state artist', this.state.artistName)
+               
             })
             .catch(err => {
                 console.log(err);
@@ -62,21 +92,21 @@ class ArtistPage extends Component {
         
                   <div className="row">
                     <div className="col-12 col-md-10 col-lg-10 mt-5">
-                      <h2 className="titleMain"></h2>
-                      <div id="followers"></div>
+                        <h2 className="titleMain">{ this.state.artistName.name}</h2>
+                      <div id="followers">Followers: { this.state.artistName.nb_fan}</div>
                       <div className="d-flex justify-content-center" id="button-container">
-                        <button
-                          className="btn btn-success mr-2 mainButton d-none"
+                        <Button
+                          className="btn btn-success mr-2 mainButton "
                           id="playButton"
                         >
                           PLAY
-                        </button>
-                        <button
-                          className="btn btn-outline-light mainButton d-none"
+                        </Button>
+                        <Button
+                          className="btn btn-outline-light mainButton"
                           id="followButton"
                         >
                           FOLLOW
-                        </button>
+                        </Button>
                       </div>
                     </div>
                   </div>
@@ -86,7 +116,29 @@ class ArtistPage extends Component {
                         <h2 className="text-white font-weight-bold">Tracks</h2>
                       </div>
                       <div className="pt-5 mb-5">
-                        <div className="row" id="apiLoaded"></div>
+                        <div className="row" id="apiLoaded">
+                            {this.state.tracksresponse.map(tracks=>{
+                                return(
+                                    <div className="col-sm-auto col-md-auto text-center mb-5" key={tracks.id}>
+                                <a href="">
+                                  <img
+                                    className="img-fluid"
+                                    src={tracks.album.cover_medium}
+                                    alt="1"
+                                  />
+                                </a>
+                                <p>
+                                  <a href=""> Track: {tracks.title_short}</a>
+                                  <br />
+                                  <a href="">Album: {tracks.album.title}</a>
+                                </p>
+                              </div>
+                                    
+                                )
+                                
+                            })}
+                        
+                        </div>
                       </div>
                     </div>
                   </div>
